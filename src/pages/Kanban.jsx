49546430a9e9568ce05d341bbdca0328e8863116
Goga <gogaimerlishvili1@gmail.com>
2022-07-25 01:31:45 +0400

    added kanban page

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -7,17 +7,17 @@ import {Header} from "../components"
 const Kanban = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category="App" title="Kanbar" />
+      <Header category="App" title="Kanban" />
       <KanbanComponent
       id="kanban"
       dataSource={kanbanData}
-      cardSettings={{contentField: 'Summary', headerField: "Id"}}
+      cardSettings={{contentField:'Summary', headerField:"Id"}}
       keyField="Status"
       >
         <ColumnsDirective>
-        {kanbanGrid.map((item,index) => (
+        {kanbanGrid.map((item,index) => 
           <ColumnDirective key={index} {...item} />
-        ))}
+        )}
         </ColumnsDirective>
       </KanbanComponent>
     </div>
