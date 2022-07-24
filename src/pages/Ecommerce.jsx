e82461a39bd821dc8ea0aab1ad5b778a59483218
Goga <gogaimerlishvili1@gmail.com>
2022-07-25 03:18:17 +0400

    added dark && light theme context and functions

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs'
 import {GoPrimitiveDot} from "react-icons/go"
 import {Stacked,Pie,Button,SparkLine} from "../components"
+import { useStateContext } from '../contexts/ContextProvider'
 import {earningData,SparklineAreaData,ecomPieChartData} from "../data/dummy"
  
 
 const Ecommerce = () => {
+  const {currentColor} = useStateContext()
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
@@ -20,7 +22,7 @@ const Ecommerce = () => {
           </div>
           <div className='mt-6'>
           <Button 
-          color="white" bgColor="blue" 
+          color="white" bgColor={currentColor}
           text="Download" 
           borderRadius="10px"
           size="md"/>
@@ -85,17 +87,17 @@ const Ecommerce = () => {
 </div> 
         <div className='mt-5'>
           <SparkLine 
-          currentColor="blue"
+          currentColor={currentColor}
           id="line-sparkline"
           type="Line"
           height="80px"
           width="250px"
           data={SparklineAreaData}
-          color="blue"
+          color={currentColor}
           />
           </div>
           <div className="mt-10">
-            <Button color="white" bgColor="blue" text="Download Report" borderRadius="10px" />
+            <Button color="white" bgColor={currentColor} text="Download Report" borderRadius="10px" />
             </div>      
                 </div>
             <div>
